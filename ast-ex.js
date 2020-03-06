d5bfc61697449810e75a0c1ebc734072b1740d87
vrivasdev <victor.rivas.sistemas@gmail.com>
2020-03-06 12:37:23 -0400

    Abstract Syntax Tree

---
+++ ast-ex.js
@@ -0,0 +1,22 @@
+// Abstract Syntax Tree: https://astexplorer.net/
+export default function(context) {
+    return {
+      VariableDeclaration(node) {
+        if (node.kind === 'const') {
+          const declaration = node.declarations[0];
+          
+          if (typeof declaration.init.value === "number") {
+            if (declaration.id.name !== declaration.id.name.toUpperCase()) {
+              context.report({
+                node: declaration.id,
+                message: "El nombre de la constante debe estar en mayuscula",
+                fix: function(fixer) {
+                  return fixer.replaceText(declaration.id, declaration.id.name.toUpperCase())
+                }
+              })
+            }
+          }
+        }
+      }
+    };
+  };
