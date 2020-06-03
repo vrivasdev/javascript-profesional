c728aa89bcd33e22d3cf074a73a49f860ad84f81
Victor Rivas <victor.rivas.sistemas@gmail.com>
2020-06-03 11:36:12 -0400

    Return type functions

--- exercises/typescript/index.ts
+++ exercises/typescript/index.ts
@@ -35,3 +35,27 @@ test = { type: 'Wildcard' };
 
 // Object
 let res: object = { type: 'Wildcard' };
+
+// ******************* Functions *********************
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+const sum = add(4, 6)
+
+function createAdder(a: number): (number) => number {
+    return function(b: number) {
+        return b + a;
+    }
+}
+
+const addFour = createAdder(4);
+const fourPlusSix = addFour(6);
+
+function fullName(firstName: string, lastName: string = 'Smith'): string { // lastName could be undefined
+    return `${firstName} ${lastName}`;
+}
+
+const victor = fullName('Victor');
+console.log(victor);
+
