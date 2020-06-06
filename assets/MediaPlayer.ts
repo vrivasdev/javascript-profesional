7a0388127a5510309a27375b225b3212f48da36f
Victor Rivas <victor.rivas.sistemas@gmail.com>
2020-06-06 12:36:48 -0400

    Typescript refactoring

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,34 +1,30 @@
 class MediaPlayer {
+    media: HTMLMediaElement
+    plugins: Array<any>
+
     constructor(config) {
         this.media = config.el
         this.plugins = config.plugins || []
-        this._initPlugins()
+        this.initPlugins()
     }
-    _initPlugins() {
-        const player = {
-            play: () => this.play(),
-            pause: () => this.pause(),
-            media: this.media,
-            get muted() {
-                return this.media.muted
-            },
-            set muted(value) {
-                this.media.muted = value
-            }
-        }
+    private initPlugins() {
         this.plugins.forEach(plugin => {
-            plugin.run(player)
+            plugin.run(this)
         })
     }
+
     play() {
         this.media.play()
     }
+
     pause() {
         this.media.pause()
     }
+
     isPaused() {
         return this.media.paused
     }
+
     togglePlay() {
         if (this.media.paused) {
             this.play()
@@ -37,12 +33,15 @@ class MediaPlayer {
             this.pause()
         }
     }
+
     toggleMute() {
         this.media.muted ? this.unmute() : this.mute()
     }
+
     mute() {
         this.media.muted = true
     }
+    
     unmute() {
         this.media.muted = false
     }
--- assets/index.ts
+++ assets/index.ts
@@ -1,10 +1,10 @@
-import MediaPlayer from './MediaPlayer.js'
-import AutoPlay from './plugins/AutoPlay.js'
-import AutoPause from './plugins/AutoPause.ts'
+import MediaPlayer from './MediaPlayer'
+import AutoPlay from './plugins/AutoPlay'
+import AutoPause from './plugins/AutoPause'
 
 const video = document.querySelector("video")
-const playButton = document.querySelector("#play")
-const muteButton = document.querySelector("#mute")
+const playButton:HTMLElement = document.querySelector("#play")
+const muteButton:HTMLElement = document.querySelector("#mute")
 
 const player = new MediaPlayer({ 
     el: video, 
